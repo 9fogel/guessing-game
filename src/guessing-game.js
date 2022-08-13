39dc5a1e95d0ccaa86a4fdb841e882b9c3a3464a
Vera Kirayonak <Vera_Kirayonak@epam.com>
2022-08-13 15:30:15 +0300

    complete task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+    }
 
     setRange(min, max) {
-
+      this.left = min;
+      this.right = max;
+      
     }
 
     guess() {
-
+      this.mid = this.left + Math.round((this.right - this.left) / 2);
+      this.number = this.mid;
+      return this.number;
     }
 
     lower() {
-
+      this.right = this.mid;
     }
 
     greater() {
-
+      this.left = this.mid;
     }
 }
 
